0292efc5adb8da8555d9aa5f5db345a5a9d58a4e
vnskln <37518904+vnskln@users.noreply.github.com>
2018-10-29 01:39:41 +0100

    basic config
    maven dependencies
    web.xml
    context
    test controller
    test jsp page

---
+++ src/main/java/spring/test/myApp/HomeController.java
@@ -0,0 +1,14 @@
+package spring.test.myApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	public String showTestPage() {
+		return "index";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>TESTING! ATTENTION PLEASE...</h1>
+</body>
+</html>
